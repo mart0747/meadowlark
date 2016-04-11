224785eaa081e2c63f4aa54e10c1bed08b1ee0e3
Joel B. Martins <joel.martins@gmail.com>
2016-04-11 16:39:32 -0500

    travis 3

---
+++ credentials.js
@@ -0,0 +1,8 @@
+//from xkcd generator
+module.exports = {
+    cookieSecret: 'mental serious discussion separate',
+    gmail: {
+        user: 'joel.martins@gmail.com',
+        password: 'P0rkito!',
+    }
+};
